dbf58ac93c510cb41ea66ab1d12290dd5c451741
Egor <Cost_benefit@mail.ru>
2022-04-05 21:16:01 +0300

    Добавление меток для карты вместе с координатами

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,20 +1,42 @@
 import React, { useEffect, useRef } from 'react'
 import L from 'leaflet'
 import PropTypes from 'prop-types'
+import Coords from '../../mocks/Coords'
 
 const Map = () => {
   const mapRef = useRef();
 
   useEffect(()=>{
     mapRef.current =  L.map('map', {
-      center: [48.8419, 24.0315],
-      zoom: 16
+      center: [52.3909553943508, 4.85309666406198],
+      zoom: 11
     })
 
     L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
       attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     })
     .addTo(mapRef.current)
+
+    Coords.forEach((coord) => {
+      const customIcon = L.icon({
+        iconUrl: './img/pin.svg',
+        iconSize: [27, 39]
+      })
+
+      L.marker({
+        lat: coord.lat,
+        lng: coord.lng
+      },
+      {
+        icon: customIcon
+      })
+      .addTo(mapRef.current)
+      .bindPopup(coord.title)
+
+      return () => {
+        mapRef.current.remove()
+      }
+    })
   }, [])
 
   return(
